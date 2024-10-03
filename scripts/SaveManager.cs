03730a64eab36549df7039750a46141434501c72
Watz <watztheengineer@gmail.com>
2024-10-03 18:01:55 -0400

    add singleton base for SaveManager

--- scripts/SaveManager.cs
+++ scripts/SaveManager.cs
@@ -3,5 +3,24 @@ using System;
 
 public partial class SaveManager : GodotObject
 {
+	private static SaveManager instance;
 	
+	private SaveManager(){
+		
+	}
+	
+	public static SaveManager getInstance() {
+		if (instance == null){
+			instance = new SaveManager();
+		}
+		return instance;
+	}
+	
+	public void Loadgame(){
+		
+	}
+	
+	public void SaveGame(){
+		
+	}
 }
